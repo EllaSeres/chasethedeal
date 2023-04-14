304628fd44ae0ce4ac69fa43f0a7640be9853581
Ioan Dragomir <123ioandragomir123@gmail.com>
2023-04-14 22:33:38 +0300

    Set up express app with basic logging

---
+++ src/server/index.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Load environment variables
+import * as dotenv from 'dotenv';
+dotenv.config();
+const { SERVER_PORT } = process.env;
+
+import express from 'express';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(morgan('tiny')); // Set up logging :)
+
+app.get('/', (req, res) => {
+    res.json({'hello': 'world!'});
+});
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server running on http://localhost:${SERVER_PORT} :)`);
+});
+

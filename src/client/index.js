a386b665df1338c9fa337102da4239723e9a9792
Ioan Dragomir <123ioandragomir123@gmail.com>
2023-04-23 13:41:59 +0300

    Example multiple screens

--- src/client/index.js
+++ src/client/index.js
@@ -1,2 +1,11 @@
 console.log('Hello, world! Use src/client/index.js as a starting point for your frontend scripting.');
 
+function setScreen(screen) {
+    document.querySelector('#container').classList = ['current-screen-' + screen];
+}
+
+document.querySelectorAll('[data-toscreen]').forEach(el => {
+    el.addEventListener('click', () => {
+        setScreen(el.dataset.toscreen);
+    });
+});

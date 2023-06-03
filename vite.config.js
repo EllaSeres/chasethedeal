6c4a7f48ff0bdb1d4fd5cac99258e742e580abfd
Ioan Dragomir <123ioandragomir123@gmail.com>
2023-06-04 02:16:08 +0300

    Fix stuff gone bad when depoloying

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,14 @@
 import path from 'path';
 
 export default {
-    root: path.resolve(__dirname, './src/client/')
+	root: path.resolve(__dirname, './src/client/'),
+	build: {
+		rollupOptions: {
+			input: {
+				'index.html': './src/client/index.html',
+				'game.html': './src/client/game.html',
+				'scoreboard.html': './src/client/scoreboard.html'
+			}
+		}
+	}
 }
-
